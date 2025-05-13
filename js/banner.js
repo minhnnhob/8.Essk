85f003a9f34fc8ac85d71065157e519f4109ab03
minhnnhob <minhnnwork02@gmail.com>
2025-05-13 09:19:24 +0700

    fix

--- js/banner.js
+++ js/banner.js
@@ -1,4 +1,4 @@
-const sig_items = document.querySelectorAll(".signature__item");
+// const sig_items = document.querySelectorAll(".signature__item");
 const sections2 = document.querySelector("#signature__item-2");
 const sections3 = document.querySelector("#signature__item-3");
 const sections4 = document.querySelector("#signature__item-4");
@@ -7,7 +7,7 @@ const sections4 = document.querySelector("#signature__item-4");
 
 options_sig = {
   root: null,
-  rootMargin: "0px 0px -100% 0px",
+  rootMargin: "0px 0px -40% 0px",
   thrreshold: 0,
 };
 
@@ -28,9 +28,39 @@ const observer_sig = new IntersectionObserver((entries, observer) => {
   });
 }, options_sig);
 
-sig_items.forEach((item) => {
-  observer_sig.observe(item);
-});
+options_sig_2 = {
+  root: null,
+  rootMargin: "0px 0px -10% 0px",
+  thrreshold: 0,
+};
+
+const observer_sig_2 = new IntersectionObserver((entries, observer) => {
+  entries.forEach((entry) => {
+    // console.log(entry.target);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("absolute");
+      // entry.target.classList.add("fixed");
+      entry.target.classList.remove("fixed");
+      //   sections2.classList.remove("fixed");
+      //   observer_sig.unobserve(entry.target);
+      // observerImg.unobserve(entry.target);
+    } else {
+      entry.target.classList.remove("absolute");
+      //   sections2.classList.add("fixed");
+      entry.target.classList.remove("fixed");
+    }
+  });
+}, options_sig_2);
+
+// sig_items.forEach((item) => {
+//   observer_sig.observe(item);
+// });
+
+observer_sig.observe(sections2);
+observer_sig.observe(sections3);
+observer_sig_2.observe(sections4);
+
+
 
 optionsImg = {
   root: null,
@@ -55,4 +85,6 @@ const observerImg = new IntersectionObserver((entries, observer) => {
 
 observerImg.observe(sections2);
 observerImg.observe(sections3);
-observerImg.observe(sections4);
+observerImg.observe(sections4); 
+
+
---
+++ js/home-2.js
@@ -0,0 +1,381 @@
+// const images = Array.from(document.images);
+// const links = Array.from(
+//   document.querySelectorAll('link[rel="stylesheet"], link[rel="preload"]')
+// );
+
+// const banner_title = document.querySelectorAll(".banner__title");
+// const header = document.querySelector(".header");
+// const banner_num = document.querySelector(".banner__slide-num");
+
+// gsap.set(header, {
+//   y: "-100",
+//   opacity: 0,
+// });
+
+// gsap.set(".banner__slide-num", {
+//   y: "80",
+//   opacity: 0,
+// });
+
+// gsap.set(banner_title, {
+//   x: "-100",
+//   opacity: 0,
+// });
+
+// // banner_title.forEach((title) => {
+// //     console.log(title);
+// // })
+
+// const total = images.length + links.length;
+// let loaded = 0;
+// let counter = { value: 0 };
+// let targetProgress = 0;
+// const startTime = performance.now();
+// console.log(total);
+
+// function updateProgress() {
+//   loaded++;
+//   targetProgress = Math.floor((loaded / total) * 100);
+// }
+// //Banner
+// // slide bottom to -top
+
+// gsap.ticker.add(() => {
+//   counter.value += (targetProgress - counter.value) * 0.05;
+
+//   const current = Math.floor(counter.value);
+//   //   document.querySelector(".num-test-2").textContent = current + "%";
+//   //   document.querySelector(".line-test-2").style.width = current - 10 + "%";
+
+//   gsap.to(".number-home-2", {
+//     textContent: current + "%",
+//     duration: 0.1,
+//     snap: "textContent",
+//     ease: "power2.out",
+//   });
+
+//   // Nếu đã loaded hết và đạt 100% hiển thị
+
+//   if (loaded === total && current >= 100) {
+//     const elapsed = performance.now() - startTime;
+//     // performance.now() ~ date.now()
+
+//     if (elapsed >= 1500) {
+//       //ddamr bao thoi gian load toi thieu 1.5s
+//       gsap.ticker.remove(updateProgress);
+
+//       const tl = gsap.timeline();
+
+//       tl.to(".number-home-2", {
+//         scale: 0,
+//         opacity: 0,
+//         duration: 0.4,
+//         ease: "none",
+//       });
+//       tl.to(".number-home-2", {
+//         scale: 0.6,
+//         opacity: 0,
+//         duration: 0.4,
+//         ease: "none",
+//       });
+
+//       tl.to(".block-home-2", {
+//         height: "0",
+//         duration: 0.8,
+//         ease: "power2.out",
+//         stagger: {
+//           each: 0.05,
+//           from: "start",
+//         },
+//       });
+
+//       tl.add(() => {
+//         document.querySelector(".banner").classList.remove("banner--hide");
+//         void document.querySelector(".banner").offsetHeight;
+//       }, "-=.6");
+
+//       tl.to(banner_title, {
+//         x: 0,
+//         opacity: 1,
+//         duration: 0.8,
+//         ease: "power2.out",
+//         delay: 0.8,
+//       });
+
+//       tl.add(() => {
+//         document.querySelector(".loader-home-2").style.display = "none";
+//       });
+
+//       tl.add(() => {
+//         gsap.to(header, {
+//           y: 0,
+//           opacity: 1,
+//           duration: 0.5,
+//           ease: "back.out",
+//         });
+
+//         gsap.to(banner_num, {
+//           y: 0,
+//           opacity: 1,
+//           duration: 0.5,
+//           ease: "back.out",
+//         });
+//       }, "<");
+//     }
+//   }
+//   if (loaded === total && targetProgress === 100 && counter.value >= 99.5) {
+//     counter.value = 100;
+//   }
+// });
+
+// // slide top to bottom
+// // gsap.ticker.add(() => {
+// //   counter.value += (targetProgress - counter.value) * 0.05;
+
+// //   const current = Math.floor(counter.value);
+// //   //   document.querySelector(".num-test-2").textContent = current + "%";
+// //   //   document.querySelector(".line-test-2").style.width = current - 10 + "%";
+
+// //   gsap.to(".number-home-2", {
+// //     textContent: current + "%",
+// //     duration: 0.1,
+// //     snap: "textContent",
+// //     ease: "power2.out",
+// //   });
+
+// //   // gsap.to(".line-test-2", {
+// //   //   width: current - 10 + "%",
+// //   //   duration: 0.1,
+// //   //   ease: "power2.out",
+// //   // });
+
+// //   // Nếu đã loaded hết và đạt 100% hiển thị
+// //   if (loaded === total && current >= 100) {
+// //     const elapsed = performance.now() - startTime;
+
+// //     // performance.now() ~ date.now()
+
+// //     if (elapsed >= 1500) {
+// //       //ddamr bao thoi gian load toi thieu 1.5s
+// //       gsap.ticker.remove(updateProgress);
+
+// //       const tl = gsap.timeline();
+
+// //       tl.to(".number-home-2", {
+// //         scale: 0,
+// //         opacity: 0,
+// //         duration: 0.4,
+// //         ease: "none",
+
+// //       });
+// //       tl.to(".number-home-2", {
+// //         scale: .6,
+// //         opacity: 0,
+// //         duration: 0.4,
+// //         ease: "none",
+
+// //       });
+
+// //       tl.to(".block-home-2", {
+// //         y: "100%",
+// //         duration: 0.8,
+// //         ease: "power1.out",
+// //         stagger: {
+// //           each: 0.05,
+// //           from: "end",
+// //         },
+// //       });
+// //     }
+// //   }
+// //   if (loaded === total && targetProgress === 100 && counter.value >= 99.5) {
+// //     counter.value = 100;
+// //   }
+// // });
+
+// images.forEach((image) => {
+//   if (image.complete) {
+//     updateProgress();
+//   } else {
+//     image.addEventListener("load", updateProgress);
+//     image.addEventListener("error", updateProgress);
+//   }
+// });
+
+// links.forEach((link) => {
+//   if (link.sheet) {
+//     updateProgress();
+//   } else {
+//     link.addEventListener("load", updateProgress);
+//     link.addEventListener("error", updateProgress);
+//   }
+// });
+
+//////////////////////////Split text
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.fonts.ready.then(() => {
+    const split = new SplitText(".mau-home-2__slogan", {
+      types: "words, chars",
+      tagName: "span",
+      mask: "words",
+    });
+
+    gsap.from(split.words, {
+      scrollTrigger: {
+        trigger: ".mau-home-2__slogan",
+        start: "top 80%",
+        // markers: true,
+        toggleActions: "play none none none",
+      },
+      y: 10,
+      opacity: 0,
+      stagger: 0.03,
+      duration: 0.4,
+      ease: "power1.out",
+    });
+
+    const split_2 = new SplitText(".mau-home-2__qoute", {
+      types: "words, chars",
+      tagName: "span",
+      mask: "words",
+    });
+
+    gsap.from(split_2.words, {
+      scrollTrigger: {
+        trigger: ".mau-home-2__qoute",
+        start: "top 75%",
+        // markers: true,
+        toggleActions: "play none none none",
+      },
+      y: 20,
+      opacity: 0,
+      stagger: 0.03,
+      duration: 0.3,
+      ease: "power2.out",
+    });
+
+    const split_3 = new SplitText(".mau-home-2__big-description", {
+      types: "words, chars",
+      tagName: "span",
+      mask: "words",
+    });
+
+    gsap.from(split_3.words, {
+      scrollTrigger: {
+        trigger: ".mau-home-2__big-description",
+        start: "top 75%",
+        // markers: true,
+        toggleActions: "play none none none",
+      },
+      y: 20,
+      opacity: 0,
+      stagger: 0.03,
+      duration: 0.3,
+      ease: "power2.out",
+    });
+  });
+});
+
+// Ensure DOM is ready
+document.addEventListener("DOMContentLoaded", () => {
+  gsap.registerPlugin(ScrollTrigger);
+
+  // const cards = gsap.utils.toArray(".signature__item");
+
+  const card1 = document.querySelector("#signature__item-1");
+  const card2 = document.querySelector("#signature__item-2");
+  const card3 = document.querySelector("#signature__item-3");
+  const card4 = document.querySelector("#signature__item-4");
+
+  console.log(card1);
+
+  const cards = [card1, card2, card3, card4];
+
+  // STEP 1: Pin Card 1
+  // ScrollTrigger.create({
+  //   trigger: card1,
+  //   start: "top top",
+  //   pin: true,
+  //   pinSpacing: false,
+  //   markers: true,
+  //   scrub: true,
+  // });
+
+  gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: card1,
+        start: "top top",
+        pin: true,
+        pinSpacing: false,
+        markers: true,
+        // scrub: true,
+        
+      },
+    })
+    .fromTo(
+      card2,
+      {
+        scale: 0.8,
+        yPercent: 0,
+        opacity: 1,
+      },
+      {
+        scale: 0.4,
+        yPercent: -60,
+        opacity: 1,
+        duration: 1.2,
+        ease: "power2.out",
+      }
+    );
+
+  gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: card2,
+        start: "-50% 70%",
+        // end:"bottom bottom",
+        end: "+=100%",
+        pin: true,
+        pinSpacing: false,
+        markers: true,
+        id: "hehe",
+        scrub: true,
+      },
+    })
+    .to(card2, {
+      scale: 1,
+      yPercent: -200,
+      opacity: 1,
+      // duration: 1.2,
+    })
+    .fromTo(
+      card3,
+      {
+        scale: 0.8,
+        yPercent: 0,
+        opacity: 1,
+      },
+      {
+        scale: 0.4,
+        yPercent: -80,
+        opacity: 1,
+        duration: 1.2,
+        ease: "power2.out",
+      }
+    );
+
+ gsap.timeline({
+    scrollTrigger: {
+      trigger: card3,
+      start: "-50% 65%",
+      // end:"bottom bottom",
+      end: "+=100%",
+      pin: true,
+      pinSpacing: false,
+      markers: true,
+      id: "hehe----2",
+      scrub: true,
+    },
+  });
+});
